ebcb8cdbea42de1d15240554a32a99a79db8bc60
bhlezzdmplg <yharrasallyguiao@gmail.com>
2024-11-05 09:57:47 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,11 @@
+// Collecting user information and storing them in variables
+let fullName = "Bhlezz Garbin";
+let age = 19;
+let favoriteNumber = 24;
+let favoriteColor = "Purple";
+
+// Logging the information to the console
+console.log("Full Name: " + fullName);
+console.log("Age: " + age);
+console.log("Favorite Number: " + favoriteNumber);
+console.log("Favorite Color: " + favoriteColor);
